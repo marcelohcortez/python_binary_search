e4864cf109aa7f366732f37c68aa7eb9ce0bd7fe
Marcelo Henriques Cortez <marcelohcortez@gmail.com>
2022-02-13 10:26:55 +0000

    Fix the comment

--- list.py
+++ list.py
@@ -1,3 +1,3 @@
 import random
 
-listOfNumbers = sorted([random.randrange(1, 50000, 1) for i in range(10000)]) #generate a random list of numbers with 10.000 numbers from 1 to 50.000 and sort them in order
+listOfNumbers = sorted([random.randrange(1, 50000, 1) for i in range(10000)]) #generate a random list of 10.000 numbers from 1 to 50.000 and sort them in order
